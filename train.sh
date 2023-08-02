0655a8c10c7b2f7bfb4f3a709f8a4401f5dbb437
Junhui Ji <junhui.ji@aminer.cn>
2023-08-02 14:56:37 +0800

    init main

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@
 # command for this script: srun -N 2 --gres=gpu:2 --ntasks-per-node=2 --cpus-per-task=4 --job-name=slurm_example --partition=dev --time=00:10:00 --output=slurm_example.out --error=slurm_example.err ./single_launch.sh
 
 # if SLURM defined, set by SLURM environment
-module load cuda/11.7
+#module load cuda/11.7
 
 export WORLD_SIZE=${SLURM_NTASKS:-1}
 export RANK=${SLURM_PROCID:-0}
@@ -25,8 +25,8 @@ export NCCL_IB_DISABLE=0
 export NCCL_IB_HCA=mlx5_0,mlx5_2
 export NCCL_SOCKET_IFNAME=bond0
 export NCCL_DEBUG=INFO
-export HF_HOME=/nxchinamobile2/shared/official_pretrains/hf_home
-export SAT_HOME=/nxchinamobile2/shared/official_pretrains/sat_home
+export HF_HOME=/gpfs/gpfs1/zphz/hf_home
+export SAT_HOME=/gpfs/gpfs1/zphz/sat_home
 #export LD_LIBRARY_PATH=/data/apps/source/nccl/build/lib/:$LD_LIBRARY_PATH
 
 # python pseudo_training.py --world_size $WORLD_SIZE --rank $RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT --local_rank $LOCAL_RANK
