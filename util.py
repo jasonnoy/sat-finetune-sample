1ba4356e494e52bad08d79f07d6a28345d38c553
Junhui Ji <junhui.ji@aminer.cn>
2023-08-08 16:19:11 +0800

    update en to zh

--- util.py
+++ util.py
@@ -9,18 +9,18 @@ if __name__ == "__main__":
     texts = []
     details = []
     keys = []
-    with open("sat_zh_2_cn_100.jsonl", "r", encoding="utf-8") as f:
+    with open("/nxchinamobile2/shared/wy/data/input/rankv3_short_new.jsonl", "r", encoding="utf-8") as f:
         for i, line in enumerate(f):
             data = json.loads(line)
-            prompts.append(data["prompt"])
-            texts.append(data["txt"])
-            details.append(data["details"])
+            prompts.append(data["prompt_en"])
+            texts.append(data["txt_en"])
+            details.append(data["details_en"])
             keys.append(data["__key__"])
     f.close()
     df = {"key": keys, "prompt": prompts, "txt": texts, "details": details}
     df = pd.DataFrame(df)
     df = df.sort_values(by='key')
-    df.to_csv("./sat_zh_50.csv", index=False, encoding="utf-8")
+    df.to_csv("./chat_sat_zh_50.csv", index=False, encoding="utf-8")
 
 # if __name__ == "__main__":
 #     df = pd.read_csv("sat_zh_50.csv")
