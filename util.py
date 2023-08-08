8f7dd6015d60b122550e10c09fe9dd70fd0b0243
Junhui Ji <junhui.ji@aminer.cn>
2023-08-08 16:10:40 +0800

    update en to zh

--- util.py
+++ util.py
@@ -3,59 +3,58 @@ import random
 import pandas as pd
 
 
-# if __name__ == "__main__":
-#     # 读取jsonl文件
-#     prompts = []
-#     texts = []
-#     details = []
-#     keys = []
-#     with open("/nxchinamobile2/shared/img_datasets/cleaned_instructions/image_caption/diy_caption_v1/meta.jsonl", "r", encoding="utf-8") as f:
-#         for i, line in enumerate(f):
-#             data = json.loads(line)
-#             prompts.append(data["prompt"])
-#             texts.append(data["txt"])
-#             details.append(data["details"])
-#             keys.append(data["__key__"])
-#             if i == 50:
-#                 break
-#     f.close()
-#     df = {"key": keys, "prompt": prompts, "txt": texts, "details": details}
-#     df = pd.DataFrame(df)
-#     df.to_csv("./sat_zh_50.csv", index=False, encoding="utf-8")
-
 if __name__ == "__main__":
-    df = pd.read_csv("sat_zh_50.csv")
-    test_ids = df['key'].to_list()
-    p = 0.75
-    with open("train_zh_100k.jsonl", 'r', encoding='utf-8') as f1, open("mix_combine_zh.jsonl", 'w', encoding='utf-8') as f2:
-        prompt = ""
-        response = ""
-        for line in f1:
+    # 读取jsonl文件
+    prompts = []
+    texts = []
+    details = []
+    keys = []
+    with open("sat_zh_2_cn_100.jsonl", "r", encoding="utf-8") as f:
+        for i, line in enumerate(f):
             data = json.loads(line)
-            prompt += " " + data["prompt"]
-            response += " " + data["response"]
-            if random.random() < p:  # concat the next sentence
-                continue
-            f2.write(json.dumps({"prompt": prompt, "response": response}, ensure_ascii=False) + "\n")
-            prompt = ""
-            response = ""
-    f2.close()
-    f1.close()
+            prompts.append(data["prompt"])
+            texts.append(data["txt"])
+            details.append(data["details"])
+            keys.append(data["__key__"])
+    f.close()
+    df = {"key": keys, "prompt": prompts, "txt": texts, "details": details}
+    df = pd.DataFrame(df)
+    df = df.sort_values(by='key')
+    df.to_csv("./sat_zh_50.csv", index=False, encoding="utf-8")
 
-    with open("/nxchinamobile2/shared/wy/data/input/rankv3_short_new.jsonl", 'r', encoding='utf-8') as f1, open("mix_combine_zh.jsonl", 'a', encoding='utf-8') as f2:
-        for line in f1:
-            data = json.loads(line)
-            if data['__key__'] in test_ids or data['status'] != 'success':
-                continue
-            f2.write(json.dumps({'prompt': data['prompt'], 'response': data['prompt_en']}, ensure_ascii=False) + "\n")
-            f2.write(json.dumps({'prompt': data['txt'], 'response': data['txt_en']}, ensure_ascii=False) + "\n")
-    f1.close()
-    f2.close()
-
-    with open("mix_combine_zh.jsonl", 'r', encoding='utf-8') as f1, open("shuffle_combine_zh.jsonl", 'a', encoding='utf-8') as f2:
-        datas = [json.loads(line) for line in f1]
-        random.shuffle(datas)
-        for data in datas:
-            f2.write(json.dumps(data, ensure_ascii=False) + "\n")
-    f1.close()
-    f2.close()
+# if __name__ == "__main__":
+#     df = pd.read_csv("sat_zh_50.csv")
+#     test_ids = df['key'].to_list()
+#     p = 0.75
+#     with open("train_zh_100k.jsonl", 'r', encoding='utf-8') as f1, open("mix_combine_zh.jsonl", 'w', encoding='utf-8') as f2:
+#         prompt = ""
+#         response = ""
+#         for line in f1:
+#             data = json.loads(line)
+#             prompt += " " + data["prompt"]
+#             response += " " + data["response"]
+#             if random.random() < p:  # concat the next sentence
+#                 continue
+#             f2.write(json.dumps({"prompt": prompt, "response": response}, ensure_ascii=False) + "\n")
+#             prompt = ""
+#             response = ""
+#     f2.close()
+#     f1.close()
+#
+#     with open("/nxchinamobile2/shared/wy/data/input/rankv3_short_new.jsonl", 'r', encoding='utf-8') as f1, open("mix_combine_zh.jsonl", 'a', encoding='utf-8') as f2:
+#         for line in f1:
+#             data = json.loads(line)
+#             if data['__key__'] in test_ids or data['status'] != 'success':
+#                 continue
+#             f2.write(json.dumps({'prompt': data['prompt'], 'response': data['prompt_en']}, ensure_ascii=False) + "\n")
+#             f2.write(json.dumps({'prompt': data['txt'], 'response': data['txt_en']}, ensure_ascii=False) + "\n")
+#     f1.close()
+#     f2.close()
+#
+#     with open("mix_combine_zh.jsonl", 'r', encoding='utf-8') as f1, open("shuffle_combine_zh.jsonl", 'a', encoding='utf-8') as f2:
+#         datas = [json.loads(line) for line in f1]
+#         random.shuffle(datas)
+#         for data in datas:
+#             f2.write(json.dumps(data, ensure_ascii=False) + "\n")
+#     f1.close()
+#     f2.close()
