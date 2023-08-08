cb04590cca06c9764bfa08c6ff0eb815c5cc2fc2
Junhui Ji <junhui.ji@aminer.cn>
2023-08-08 10:15:03 +0800

    update en to zh

--- chat_sat_zh_2_en.py
+++ chat_sat_zh_2_en.py
@@ -1,3 +1,5 @@
+import json
+
 import torch
 from sat import AutoModel
 from transformers import AutoTokenizer
@@ -14,7 +16,7 @@ class FineTuneModel(ChatGLM2Model):
         super().__init__(args, transformer=transformer, parallel_output=parallel_output, **kw_args)
         self.add_mixin("lora", LoraMixin(args.num_layers, 10), reinit=True)
 
-def chat(query, model, tokenizer, 
+def chat_response(query, model, tokenizer,
         max_length: int = 256, num_beams=1, top_p=0.7, top_k=0, temperature=0.95):
     prompt = f"[Round 0]\n\n问：{query}\n\n答："
     inputs = tokenizer([prompt], return_tensors="pt").to(model.parameters().__next__().device)['input_ids'][0]
@@ -36,7 +38,8 @@ def chat(query, model, tokenizer,
     output_list = list(output)
 
     response = tokenizer.decode(output_list[0])
-    print(response)
+    return response
+
 
 if __name__ == "__main__":
     import argparse
@@ -56,7 +59,7 @@ if __name__ == "__main__":
     # model, model_args = ChatGLM2Model.from_pretrained('/nxchinamobile2/shared/dm/snapbatches/1690296760278/checkpoints/finetune-chatglm2-6b-07-25-22-53',
     # model, model_args = ChatGLM2Model.from_pretrained('/nxchinamobile2/shared/jjh/projects/ftsample/checkpoints/finetune-chatglm2-6b-07-27-10-19',
     # model, model_args = ChatGLM2Model.from_pretrained('/nxchinamobile2/shared/jjh/projects/ftsample/checkpoints/finetune-chatglm2-6b-07-28-10-26',
-    model, model_args = ChatGLM2Model.from_pretrained('/nxchinamobile2/shared/jjh/projects/sat-finetune-sample/checkpoints/finetune-chatglm2-6b-08-07-11-23',
+    model, model_args = ChatGLM2Model.from_pretrained('/nxchinamobile2/shared/jjh/projects/sat-finetune-sample/checkpoints/finetune-chatglm2-6b-08-07-19-39',
     args=argparse.Namespace(
         fp16=True,
         skip_init=True,
@@ -67,9 +70,17 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
     
     model.add_mixin('auto-regressive', CachedAutoregressiveMixin())
-    qlist = [
-            'Pro Taper Rear Sprocket for 2008 KTM 505SXF Sunstar Aluminum Rear Sprocket. ', 'Illustration of facades in Paris. ', "Hand-engraved baccarat paperweight, baccarat crystal, cherry blossom, heart-shaped paperweight, collector's gift for women. ", 'The price of iron ore has plunged from $120 a tonne 12 months ago to $63 tonne a tonne. ', 'Dancers performing on stage from the Creative Arts Dance Academy during Andover Day in downtown Andover. ', 'A&P Christmas Vinyl Record Albums transferred to CD. ', '"I Want You To Give Me A Discount Tote Bag" ', 'A photo of Palm Beach Gardens, FL, with a Latin band performing. ', 'Maytag 30" Built-In Single Electric Convection Wall Oven in Stainless steel. ', 'JCB 8014 CTS MINI DIGGER, YEAR 2016, with 935 hours, complete with digging bucket. ', 'A crop of a girl wearing yellow protective rubber gloves, wiping furniture. ', 'Private issue SAHARA TAHOE Lounge Act comedian signed late 70s. ', "Men's Knight Left Hand Golf Clubs with Bag & Cart Belleville Belleville Area image. ", 'Free shipping on 2*30cm*100cm 11color Auto Flash Point Film Tail light Tint Vinyl Film sticker. ', 'Turbofan E23D3/2C - Half Size Digital Electric Convection Ovens with a Stainless Steel Base Stand. ', 'Google Algorithm Updates and Changes infographic about social media. ', 'Dr. Bonnie Henry, regional health officer for B.C., discussing the potential impact of the mandatory mask order. ', "Women's cute fruit food socks for coffee, avocado, apple, and cherry Hamburger eggs. ", 'Angel Sleeve Blouse styles and fashion inspiration. ', "Transparent door stopper buffer for furniture protection in children's bedroom and kitchen. ", 'Premium felt cat cave bed for large cats and kittens. ', 'Electro Essence 30ml ABFE from Broome Natural Wellness. ', 'Blue beach umbrellas at Point Of Rocks, Crescent Beach, Siesta Key. ', 'Peach Indian Jacquard Brocade Wedding Dress Fabric. ', '"The Inventor and the Tycoon: The Murderer Eadweard Muybridge, the Entrepreneur Leland Stanford, and the Birth of Moving Pictures" book. ', 'Thermaltake Radiator Pacific CL480 (480mm, 5x G 1/4, black metal). ', 'A juvenile striped skunk captured in Lexington, KY. ', 'Kobe iPad 2 stand case with port designs. ', 'Single Family Home For Sale: 6363 Cedar Sage Trail. ', 'Breakfast pizza with cheddar, kale, and bacon. ', 'Not a caption', 'After Mein Kampf (1940) and Here Is Germany. ', 'Double outdoor spot-luminaire Helia LED IP55 H10 cm-white outdoor wall lamp. ', 'Jesus Christ crucified on the cross. ', '"1992 Country Christmas: An Old Fashioned Christmas" hardcover book with jacket. ', 'The property located at 106 N 181st St in Shoreline was sold for $745,000. I represented the buyer. ', 'Kiddy Guardianfix Pro 2-3 car seat in Racing Black. ', 'INFINITE 2013 calendar wallpaper collection. '
-            ]
 
-    for q in tqdm(qlist):
-        chat(q, model, tokenizer, max_length=args.max_length, num_beams=args.num_beams, top_p=args.top_p, temperature=args.temperature, top_k=args.top_k)
+    input_path = "/nxchinamobile2/shared/img_datasets/cleaned_instructions/image_caption/diy_caption_v1/meta.jsonl"
+    output_path = "/nxchinamobile2/shared/jjh/projects/sat-finetune-sample/sat_zh_2_cn_100.jsonl"
+    with open(input_path, 'r', encoding='utf-8') as f1, open(output_path, 'w', encoding='utf-8') as f2:
+        for i, line in enumerate(tqdm(f1)):
+            data = json.loads(line)
+            data['prompt'] = chat_response(data['prompt'], model, tokenizer, max_length=args.max_length, num_beams=args.num_beams, top_p=args.top_p, temperature=args.temperature, top_k=args.top_k)
+            data['txt'] = chat_response(data['txt'], model, tokenizer, max_length=args.max_length, num_beams=args.num_beams, top_p=args.top_p, temperature=args.temperature, top_k=args.top_k)
+            data['details'] = chat_response(data['details'], model, tokenizer, max_length=args.max_length, num_beams=args.num_beams, top_p=args.top_p, temperature=args.temperature, top_k=args.top_k)
+            f2.write(json.dumps(data, ensure_ascii=False) + '\n')
+            if i == 100:
+                break
+    f1.close()
+    f2.close()
