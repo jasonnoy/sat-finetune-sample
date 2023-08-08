fe6b4a2eb6e073a70527099639a541e82be3fbb0
Junhui Ji <junhui.ji@aminer.cn>
2023-08-08 12:04:42 +0800

    update en to zh

--- util.py
+++ util.py
@@ -7,12 +7,14 @@ if __name__ == "__main__":
     prompts = []
     texts = []
     details = []
+    keys = []
     with open("/nxchinamobile2/shared/img_datasets/cleaned_instructions/image_caption/diy_caption_v1/meta.jsonl", "r", encoding="utf-8") as f:
         for i, line in enumerate(f):
             data = json.loads(line)
             prompts.append(data["prompt"])
             texts.append(data["txt"])
             details.append(data["details"])
+            keys.append(data["__key__"])
             if i == 50:
                 break
     f.close()
