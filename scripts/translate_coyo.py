4f21b41af789127ff57047132f82582d193805c9
Junhui Ji <junhui.ji@aminer.cn>
2023-08-11 14:27:54 +0800

    init sbatch translate

--- scripts/translate_coyo.py
+++ scripts/translate_coyo.py
@@ -70,7 +70,10 @@ def infer(inputs, model, tokenizer, num_beams=1, top_p=0.7, temperature=0.95, ba
             batch_size=1,
             strategy=strategy
         )[0]
-        output_list.append(tokenizer.decode(output))
+        print("output:", output)
+        response = tokenizer.decode(output)
+        print(response)
+        output_list.append(response)
 
     return output_list
 
